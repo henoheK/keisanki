70f0611dd45bf45297e27891aaaf20f111804d25
henohenon <takeru_20n1100821@nnn.ed.jp>
2020-05-25 05:53:51 +0900

    は、は、疲れるわ

---
+++ keisan.js
@@ -0,0 +1,120 @@
+var num =0;
+var keis_kigo=['+','-','*','/'];
+const keisan_siki_Imput=document.getElementById('keisan_siki');
+const jikou_button=document.getElementById('jikkou');
+const resultDivided=document.getElementById('result-area');
+var valies=new Array();
+var how_kei=new Array();
+jikou_button.onclick = () => 
+{
+    var imput_siki=keisan_siki_Imput.value;
+    var result=keisan_start(imput_siki);
+    hyoj_keka(result);
+    }
+function hyoj_keka(result)
+{
+    removeAllChildren(resultDivided)
+    const header = document.createElement('h1');
+    header.innerText = '計算結果';
+    header.className='text';
+    resultDivided.appendChild(header);
+  
+    const kaigyo = document.createElement('br');
+    const paragraph = document.createElement('h1');
+    paragraph.innerText = result;
+    paragraph.className='text';
+    resultDivided.appendChild(paragraph);
+}
+  function removeAllChildren(element) {
+    while (element.firstChild) {
+      // 子どもの要素があるかぎり除去
+      element.removeChild(element.firstChild);
+    }
+  }
+  function keisan_start(values)
+{
+    //計算を実行
+    sta_set();
+    sujies(values);
+    return keisan();
+}
+function sta_set()
+{
+    num=0;
+    valies.length=0;
+    how_kei.length=0;
+}
+function keisan()
+{
+    var result=(valies[0]);
+    for (var i=1;i< valies.length;i++)
+    {
+        //いろんな計算
+        if(how_kei[i-1]==0)
+        {
+            result+=(valies[i]);
+        }
+        else if(how_kei[i-1]==1)
+        {
+            result-=(valies[i]);
+        }
+        else if(how_kei[i-1]==2)
+        {
+            result*=(valies[i]);
+        }
+        else if(how_kei[i-1]==3)
+        {
+            result/=(valies[i]);
+        }}
+    return result;
+}
+function sujies(valuearray)
+{
+    //区切らせる
+    // foreach(value in valuearray)
+    //{
+        //半角処理  
+        //  han_cha_zen(setvalary[num])
+        //}
+        //ここまでに、数値型に直すこと
+    while(num<valuearray.length)
+    {
+        valies.push(get_one_word(valuearray));
+        //いろんな計算記号判定
+        how_kei.push(keis_kigo.indexOf(valuearray[num]));
+        num+=1;
+        if(num>=valuearray.length)
+        {
+            break;
+        }
+    }
+}
+function get_one_word(setvalary)
+{
+    //ひと数ごとに分ける
+    var oneword=0;
+    //いろんな計算
+    //数字か、＋とかじゃないかを判別
+    while(!keis_kigo.includes(setvalary[num]))
+    {
+        oneword=keta_up(oneword,setvalary[num]);
+        num+=1;
+        //最後の文字だったら出る処理
+        if(num>=setvalary.length)
+        {
+            break;
+        }
+    }
+    return oneword
+}
+function keta_up(old_val,add_val)
+{
+    return (old_val*10)+Number(add_val);
+}
+/*** 
+function han_cha_zen(set_val_str) {
+    return str.replace(/[０-９]/g, function(s) {
+        return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
+    });
+}
+***/
